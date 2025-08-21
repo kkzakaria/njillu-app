fa472d225f306a812534607108506da942565f9b
kkzakaria <kkzakaria@outlook.com>
2025-08-21 10:00:42 +0000

    Suppression des travaux de la page clients

--- app/[locale]/clients/components/client-create-page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { ClientForm } from './client-form';
-
-export function ClientCreatePage() {
-  return (
-    <ClientForm mode="create" />
-  );
-}
--- app/[locale]/clients/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function Page() {
-  // Redirect to active clients by default
-  redirect('/clients/active');
-}
